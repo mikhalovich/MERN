f8072aae4d683a793bec4ba55ab9fce5308d550d
mikhalovich <z-m@tut.by>
2020-02-17 00:01:42 +0300

    Add new model User

---
+++ routes/auth.routes.js
@@ -0,0 +1,21 @@
+const { Router } = require('express');
+const User = require('../models/User');
+const router = Router();
+
+// /api/auth/register
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong, try again later' });
+  }
+});
+
+// /api/auth/login
+router.post('/login', async (req, res) => {
+  
+});
+
+module.exports = router; 
