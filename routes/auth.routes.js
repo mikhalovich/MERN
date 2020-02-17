f828b317888cd4ffaef67558f6e2c582d3641462
mikhalovich <z-m@tut.by>
2020-02-17 08:52:37 +0300

    Add registration functionality

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = Router();
 
@@ -6,8 +7,18 @@ const router = Router();
 router.post('/register', async (req, res) => {
   try {
     const { email, password } = req.body;
+    const candidate = await User.findOne({ email });
 
+    if (candidate) {
+      return res.status(400).json({ message: 'This email already exists' });
+    }
 
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const iser = new User({ email, password: hashedPassword });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User created' });
   } catch (e) {
     res.status(500).json({ message: 'Something went wrong, try again later' });
   }
