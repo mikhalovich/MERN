b6c575c30582043b73b70608ff35fa4e901bb34c
mikhalovich <z-m@tut.by>
2020-02-17 09:10:37 +0300

    Add authorization

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
@@ -41,8 +43,47 @@ router.post(
 });
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
-  
+router.post(
+  '/login', 
+  [
+    check('email', 'Enter correct email').normalizeEmail().isEmail(),
+    check('password', 'Enter password').exists()
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Incorrect data'
+      });
+    }
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'User is not define' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Data is incorrect, try again' });
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      config.get('jwtSecret'),
+      { expiresIn: '1h' }
+    );
+    
+    res.json({ token, userId: user.id });
+
+  } catch (e) {
+    res.status(500).json({ message: 'Something went wrong, try again later' });
+  }
 });
 
 module.exports = router; 
