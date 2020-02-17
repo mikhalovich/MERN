e6d760cd9c915125638d14ff62de9b400754387c
mikhalovich <z-m@tut.by>
2020-02-17 08:58:52 +0300

    Add registration validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,27 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
+router.post(
+  '/register', 
+  [
+    check('email', 'Email is incorrect').isEmail(),
+    check('password', 'Min length is 6').isLength({ min: 6 })
+  ],
+  async (req, res) => {
   try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Incorrect data'
+      });
+    }
+
     const { email, password } = req.body;
     const candidate = await User.findOne({ email });
 
