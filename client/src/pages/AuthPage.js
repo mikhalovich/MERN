bb711a63f0f8e551b3f37a415a72cf927e39ac98
ssavitski <sv.siarhei@gmail.com>
2020-03-15 16:40:35 +0300

    Added registration functionality

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/auth.css';
+import { useHttp } from '../hooks/http.hook';
+import { useMessage } from '../hooks/message.hook';
 
 export const AuthPage = () => {
+  const message = useMessage();
+  const { loading, request, error, clearError } = useHttp();
+  const [form, setForm] = useState({
+    email: '', password: ''
+  });
+
+  useEffect(() => {
+    message(error);
+    clearError();
+  }, [error, message, clearError]);
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register', 'POST', {...form});
+      message(data.message);
+    } catch (e) {}
+  };
+
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Cut link</h1>
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Authorization</span>
+            <div>
+              <div className="input-field">
+                <input 
+                  placeholder="Type email" 
+                  id="email" 
+                  type="text" 
+                  name="email"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                <input 
+                  placeholder="Type password" 
+                  id="password" 
+                  type="password" 
+                  name="password"
+                  className="yellow-input"
+                  onChange={changeHandler}
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+
+            </div>
+          </div>
+          <div className="card-action">
+            <button 
+              className="btn yellow darken-4 auth-btn"
+              disabled={loading}
+            >
+              Log in
+            </button>
+            <button 
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -14,33 +14,33 @@ router.post(
     check('password', 'Min length is 6').isLength({ min: 6 })
   ],
   async (req, res) => {
-  try {
-    const errors = validationResult(req);
+    try {
+      const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-        message: 'Incorrect data'
-      });
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect data'
+        });
+      }
+
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: 'This email already exists' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: 'User created' });
+    } catch (e) {
+      res.status(500).json({ message: 'Something went wrong, try again later1' });
     }
-
-    const { email, password } = req.body;
-    const candidate = await User.findOne({ email });
-
-    if (candidate) {
-      return res.status(400).json({ message: 'This email already exists' });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const iser = new User({ email, password: hashedPassword });
-
-    await user.save();
-
-    res.status(201).json({ message: 'User created' });
-  } catch (e) {
-    res.status(500).json({ message: 'Something went wrong, try again later' });
-  }
-});
+  });
 
 // /api/auth/login
 router.post(
