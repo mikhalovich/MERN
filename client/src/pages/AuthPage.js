9790a364975d1ccdbc339f33ec2f36cbf039dcae
ssavitski <sv.siarhei@gmail.com>
2020-03-16 13:45:01 +0300

    Added authentication functionality

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import '../styles/auth.css';
 import { useHttp } from '../hooks/http.hook';
 import { useMessage } from '../hooks/message.hook';
+import { AuthContext } from '../context/AuthContext';
 
 export const AuthPage = () => {
+  const auth = useContext(AuthContext);
   const message = useMessage();
   const { loading, request, error, clearError } = useHttp();
   const [form, setForm] = useState({
@@ -23,6 +25,13 @@ export const AuthPage = () => {
     try {
       const data = await request('/api/auth/register', 'POST', {...form});
       message(data.message);
+    } catch (e) {}
+  };
+
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/auth/login', 'POST', {...form});
+      auth.login(data.token, data.userId);
     } catch (e) {}
   };
 
@@ -64,6 +73,7 @@ export const AuthPage = () => {
             <button 
               className="btn yellow darken-4 auth-btn"
               disabled={loading}
+              onClick={loginHandler}
             >
               Log in
             </button>
