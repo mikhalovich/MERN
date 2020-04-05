b0eed7ae9335ca603fea9b9fca6e0e7637caa877
mikhalovich <z-m@tut.by>
2020-04-05 22:53:15 +0300

    Add Create short link page

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext';
 
 export const CreatePage = () => {
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const { request } = useHttp();
+  const [link, setLink] = useState('');
+
+  useEffect(() => {
+    window.M.updateTextFields();
+  }, []);
+
+  const pressHandler = async event => {
+    if (event.key === 'Enter') {
+      try {
+        const data = await request('/api/link/generate', 'POST', {from: link}, {
+          Authorization: `Bearer ${auth.token}`
+        }); 
+        debugger;
+        history.push(`/detail/${data.link._id}`);
+      } catch (error) {
+        
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>Create Page</h1>
+    <div className="row">
+      <div className="col s8 offset-s2" style={{ paddingTop: "2rem" }}>
+        <input
+          placeholder="Past link"
+          id="link"
+          type="text"
+          value={link}
+          onChange={e => setLink(e.target.value)}
+          onKeyPress={pressHandler}
+        />
+        <label htmlFor="link">Past link</label>
+      </div>
     </div>
-  )
+  );
 }
